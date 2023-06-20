b4a114e2513caed9b1f3f81549be4eaba915d0f4
LUYE2233 <ly112210@outlook.com>
2023-06-20 15:57:19 +0800

    注册登陆系统更新

---
+++ src/main/java/org/thefouthgroup/servlet/ResetServlet.java
@@ -0,0 +1,9 @@
+package org.thefouthgroup.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+@WebServlet("/ResetServlet")
+public class ResetServlet extends HttpServlet {
+}
